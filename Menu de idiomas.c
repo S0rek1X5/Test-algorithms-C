f56059161118a8837f519f792fc37adfbb47fe3f
Daniel <danielbebon@gmail.com>
2021-09-28 20:57:13 -0500

    Update Menu de idiomas.c

--- Menu de idiomas.c
+++ Menu de idiomas.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
-	
-	int idiom;
+	char idiom [4];
 
 	printf("Bienvenido");
-	printf("/nSeleccione idioma/Choose lenguage\nEspanol = 1 --- English = 2\n: ");
-	scanf("%d",&idiom);
-	if(idiom==1){
-  	//Contenido en español
+	printf("/nSeleccione idioma/Choose lenguage\nSpanish = A --- English = B\n: ");
+	scanf("%s",&idiom);
+	if(idiom[0]=='a' && idiom[0]=='A'){
+	//Contenido en español
 	}
-	else if(idiom=2){
+	else if(idiom[0]=='b' && idiom[0]=='B'){
 	//English content
 	}
 	else{
 	printf ("El idioma marcado no esta registrado/The lenguage selected si nota registered");
 	}
-	return 0;
+	return 0;	
 }
