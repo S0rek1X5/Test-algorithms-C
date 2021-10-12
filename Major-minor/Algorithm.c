988507e0cf43c6ee8265888c4c7ba0f2c4bd4d97
Daniel <danielbebon@gmail.com>
2021-10-11 23:13:37 -0500

    Update Algorithm.c

--- Major-minor/Algorithm.c
+++ Major-minor/Algorithm.c
@@ -2,17 +2,11 @@
 int main ()
 {				//Determina el mayor y el menor
 	float a, b, c, d, e, max, min;
-	printf("Introduzca el valor a\n: ");
-	scanf("%f",&a);
-	printf("Introduzca el valor b\n: ");
-	scanf("%f",&b);
-	printf("Introduzca el valor c\n: ");
-	scanf("%f",&c);
-	printf("Introduzca el valor d\n: ");
-	scanf("%f",&d);
-	printf("Introduzca el valor e\n: ");
-	scanf("%f",&e);
-	
+	printf("Introduzca el valor a\n: ");scanf("%f",&a);
+	printf("Introduzca el valor b\n: ");scanf("%f",&b);
+	printf("Introduzca el valor c\n: ");scanf("%f",&c);
+	printf("Introduzca el valor d\n: ");scanf("%f",&d);
+	printf("Introduzca el valor e\n: ");scanf("%f",&e);
 	max=a;
 	if(b>max){
 		max=b;
@@ -27,7 +21,6 @@ int main ()
 		max=e;
 	}
 	printf("El numero mayor es\n-> %1.1f",max);
-	
 	min=a;
 	if(b<min){
 		min=b;
